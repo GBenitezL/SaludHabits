c7ea9485c8f142d21c6ff93a1004e8f5c8d24884
Gustavo Benitez <benitezlgustavo@gmail.com>
2021-11-04 23:38:56 -0500

    Habito 7

--- SaludHabitsV2/ViewControllerHabito7.swift
+++ SaludHabitsV2/ViewControllerHabito7.swift
@@ -8,13 +8,37 @@
 import UIKit
 
 class ViewControllerHabito7: UIViewController {
-
+    
+    @IBOutlet weak var lbContadorAgua: UILabel!
+    @IBOutlet weak var pbProgreso: UIProgressView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let contador = Int(lbContadorAgua.text ?? "0") ?? 0
+        progreso(cont: contador)
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func stpContadorAgua(_ sender: UIStepper) {
+        lbContadorAgua.text = String(format: "%.0f", sender.value)
+        progreso(cont: Int(sender.value))
+        if lbContadorAgua.text == "7" {
+            let alert = UIAlertController(title: "Excelente!", message: "¡Estás completamente hidratado!", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {UIAlertAction in }))
+            self.present(alert, animated: true, completion: nil)
+
+        }
+    }
+    
+    func progreso(cont: Int){
+        if (cont >= 7){
+            pbProgreso.setProgress(1, animated: true)
+        }
+        else {
+            pbProgreso.setProgress(Float(cont)/7, animated: true)
+        }
+    }
 
     /*
     // MARK: - Navigation
