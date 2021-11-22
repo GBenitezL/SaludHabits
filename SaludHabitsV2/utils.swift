1afeca98c3bfd98a717a0dfa54ddc930d2b2664d
David García <david.garcia@kuona.co>
2021-11-22 06:07:19 -0600

    Added saves to core data at load, pending to put conditional and fetching

--- SaludHabitsV2/utils.swift
+++ SaludHabitsV2/utils.swift
@@ -6,6 +6,21 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
+
+extension Date {
+
+    var onlyDate: Date? {
+        get {
+            let calender = Calendar.current
+            var dateComponents = calender.dateComponents([.year, .month, .day], from: self)
+            dateComponents.timeZone = NSTimeZone.system
+            return calender.date(from: dateComponents)
+        }
+    }
+
+}
 
 func dataFileURLHabitos() -> URL {
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
@@ -50,4 +65,62 @@ func completarHabito(numHabito: Int, cancel: Bool = false) {
         print(" al escribir en el archivo")
     }
     
+}
+
+func recopilarRegistros() {
+    var habitos = [Habito]()
+    
+    do {
+        let data = try Data(contentsOf: dataFileURLHabitos())
+        habitos = try PropertyListDecoder().decode([Habito].self, from: data)
+    }
+    catch {
+        print("Error al cargar el archivo")
+    }
+    
+    for i in 0...8{
+        if habitos[i].activo {
+            save(completo: habitos[i].completo, fecha: habitos[i].creacion, numHabito: habitos[i].numHabito)
+            habitos[i].creacion = Date().onlyDate
+            habitos[i].completo = false
+        }
+    }
+    
+    do {
+        let data = try PropertyListEncoder().encode(habitos)
+            try data.write(to: dataFileURLHabitos())
+    }
+    catch {
+        print(" al escribir en el archivo")
+    }
+    
+}
+
+func save(completo: Bool, fecha: Date, numHabito: Int) {
+  
+  guard let appDelegate =
+    UIApplication.shared.delegate as? AppDelegate else {
+    return
+  }
+  
+  let managedContext =
+    appDelegate.persistentContainer.viewContext
+  
+  let entity =
+    NSEntityDescription.entity(forEntityName: "RegistroHabito",
+                               in: managedContext)!
+  
+  let registro = NSManagedObject(entity: entity,
+                               insertInto: managedContext)
+  
+  registro.setValue(completo, forKeyPath: "completo")
+  registro.setValue(fecha, forKeyPath: "fecha")
+  registro.setValue(numHabito, forKeyPath: "numHabito")
+  
+  do {
+    try managedContext.save()
+    // people.append(person)
+  } catch let error as NSError {
+    print("Could not save. \(error), \(error.userInfo)")
+  }
 }
