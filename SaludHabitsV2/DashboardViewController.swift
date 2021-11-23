3b5df24db14e32b13e00c9a34803a103d459aed6
Irene <truchadoirene@gmail.com>
2021-11-22 18:23:23 -0600

    images added

--- SaludHabitsV2/DashboardViewController.swift
+++ SaludHabitsV2/DashboardViewController.swift
@@ -81,16 +81,5 @@ class DashboardViewController: UIViewController, UITableViewDataSource, UITableV
         super.viewDidLoad()
         //recopilarRegistros()
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-     // Ilet storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- SaludHabitsV2/ViewControllerHabito8.swift
+++ SaludHabitsV2/ViewControllerHabito8.swift
@@ -43,14 +43,4 @@ class ViewControllerHabito8: UIViewController {
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {UIAlertAction in }))
             self.present(alert, animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
